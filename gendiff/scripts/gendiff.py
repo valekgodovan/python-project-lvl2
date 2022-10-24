3dd61524c903c898f4022eeb3a8a987b2a60ef4e
valek <valekgodovan@gmail.com>
2022-10-24 23:46:44 +0600

    result to json

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,20 +14,21 @@ def get_diff(file1, file2) -> str:
     file2_dict = get_file(file2)
     keys = file1_dict.keys() | file2_dict.keys()
     merge_dict = {}
-    result = []
+    result = {}
     for v in keys:
         merge_dict[v] = [file1_dict.get(v, []), file2_dict.get(v, [])]
     for i in sorted(merge_dict.keys()):
         if i not in file1_dict:
-            result.append(f'+ {i}: {file2_dict[i]}')
+            result[f'+ {i}'] = file2_dict[i]
         elif i not in file2_dict:
-            result.append(f'- {i}: {file1_dict[i]}')
+            result[f'- {i}'] = file1_dict[i]
         elif file1_dict[i] == file2_dict[i]:
-            result.append(f'  {i}: {file1_dict[i]}')
+            result[f'  {i}'] = file1_dict[i]
         else:
-            result.append(f'- {i}: {file1_dict[i]}')
-            result.append(f'+ {i}: {file2_dict[i]}')
-    return '\n'.join(result)
+            result[f'- {i}']= file1_dict[i]
+            result[f'+ {i}']= file2_dict[i]
+    file_json = json.dumps(result, indent=2)
+    return file_json
 
 
 def main():
