1d7584927113bbadcc77e7ed08d88bc741a21a54
valek <valekgodovan@gmail.com>
2022-10-24 20:48:02 +0600

    change result to list_string

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,20 +14,20 @@ def get_diff(file1, file2) -> str:
     file2_dict = get_file(file2)
     keys = file1_dict.keys() | file2_dict.keys()
     merge_dict = {}
-    result = ''
+    result = []
     for v in keys:
         merge_dict[v] = [file1_dict.get(v, []), file2_dict.get(v, [])]
     for i in sorted(merge_dict.keys()):
         if i not in file1_dict:
-            result += f'+ {i}: {file2_dict[i]}\n'
+            result.append(f'+ {i}: {file2_dict[i]}')
         elif i not in file2_dict:
-            result += f'- {i}: {file1_dict[i]}\n'
+            result.append(f'- {i}: {file1_dict[i]}')
         elif file1_dict[i] == file2_dict[i]:
-            result += f'  {i}: {file1_dict[i]}\n'
+            result.append(f'  {i}: {file1_dict[i]}')
         else:
-            result += f'- {i}: {file1_dict[i]}\n'
-            result += f'+ {i}: {file2_dict[i]}\n'
-    return result
+            result.append(f'- {i}: {file1_dict[i]}')
+            result.append(f'+ {i}: {file2_dict[i]}')
+    return '\n'.join(result)
 
 
 def main():
