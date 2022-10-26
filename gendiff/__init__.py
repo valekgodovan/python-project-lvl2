6916962cd09d87f837564726e01523986418f4ca
valek <valekgodovan@gmail.com>
2022-10-26 17:59:28 +0600

    make gengiff package

---
+++ gendiff/__init__.py
@@ -0,0 +1,6 @@
+from gendiff.gendiff import gen_diff, get_file
+
+__all__ = (
+    'gen_diff',
+    'get_file',
+)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,35 +1,7 @@
 #!/usr/bin/env python
 """Main program."""
 import argparse
-import json
-
-
-def get_file(name_file):
-    file = json.load(open(name_file))
-    return file
-
-
-def get_diff(file1, file2) -> str:
-    file1_dict = get_file(file1)
-    file2_dict = get_file(file2)
-    keys = file1_dict.keys() | file2_dict.keys()
-    merge_dict = {}
-    result = {}
-    for v in keys:
-        merge_dict[v] = [file1_dict.get(v, []), file2_dict.get(v, [])]
-    for i in sorted(merge_dict.keys()):
-        if i not in file1_dict:
-            result[f'+ {i}'] = file2_dict[i]
-        elif i not in file2_dict:
-            result[f'- {i}'] = file1_dict[i]
-        elif file1_dict[i] == file2_dict[i]:
-            result[f'  {i}'] = file1_dict[i]
-        else:
-            result[f'- {i}'] = file1_dict[i]
-            result[f'+ {i}'] = file2_dict[i]
-    file_json = json.dumps(result, indent=2)
-    return file_json.translate({ord('"'): None})
-
+from gendiff import gen_diff
 
 def main():
     parser = argparse.ArgumentParser(
@@ -39,7 +11,7 @@ def main():
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
-    print(get_diff(args.first_file, args.second_file))
+    print(gen_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
